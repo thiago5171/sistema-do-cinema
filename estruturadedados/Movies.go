3a376f8c42c08ade0c43a405f5e975542abb9d40
thiago5171 <Tgazaroli@gmail.com>
2021-04-29 16:29:21 -0300

    Update Movies.go

--- estruturadedados/Movies.go
+++ estruturadedados/Movies.go
@@ -9,26 +9,7 @@ type Movie struct{
 	Sessao 	    *Sessao
 }
 
-func (x Movie)  SeeMovie(){
-	fmt.Println("Filme->", x.Name);
 
-	if(x.Sessao.RoomL == "1"){
-		fmt.Println("sessão legendada");
-
-	}
-	if(x.Sessao.RoomD =="1"){
-		fmt.Println("sessão dublada");
-	}
-
-	if (x.Sessao.tarde=="1"){
-		fmt.Println("Horario de inicio: 16:00");
-		
-	}
-	if (x.Sessao.noite=="1"){
-		fmt.Println("Horario de inicio: 20:00");
-		
-	}
-}
 /*
 func NewMovie(Name string ,RoomD, RoomL string) *Movie{
 	return &Movie{Name,&Sessao{RoomD, RoomL}}
